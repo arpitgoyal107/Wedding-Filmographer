18f1824be7fbc987b75f4e203281243f77f675a6
Arpit Goyal <arpitgoyal126116@gmail.com>
2024-03-25 13:59:55 +0530

    minor footer alignment correction

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,71 +10,83 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-l from-[#D14D72] via-[#FFABAB] to-[#D14D72] text-white relative">
-      <div className=" max-w-7xl mx-auto overflow-hidden p-3">
-        <div className="w-full flex  lg:flex-row flex-wrap justify-between">
-          <div className="py-6 basis-1/4">
-            <img src="/images/White.png" className="px-6" alt="" />
-          </div>
+    <>
+      <footer className="bg-gradient-to-l from-[#D14D72] via-[#FFABAB] to-[#D14D72] text-white relative">
+        <div className=" max-w-7xl mx-auto overflow-hidden p-3">
+          <div className="w-full flex flex-row flex-wrap justify-center lg:justify-between">
+            <div className="py-6 basis-1/2 lg:basis-1/3 order-1 flex flex-col self-center">
+              <img src="/images/White.png" className="px-6 max-w-64" alt="" />
+            </div>
 
-          <div className="py-6 basis-1/4">
-            <h1 className="text-3xl font-bold text-center ">Social Media</h1>
-            <div className="flex justify-center space-x-4 mt-2">
+            <div className=" flex flex-row lg:flex-col py-6 lg:basis-1/3 order-3 lg:order-2">
+              <h1 className="text-lg md:text-3xl font-bold text-center flex  self-center pt-1 pr-1">
+                Social Media:
+              </h1>
+              <div className="flex justify-center space-x-4 mt-2">
+                <a
+                  href="https://facebook.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white bg-gradient-to-l from-[#FFABAB] from-10% to-[#D14D72] p-3 rounded-full"
+                >
+                  <FaFacebookF />
+                </a>
+                <a
+                  href="https://twitter.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white bg-gradient-to-l from-[#FFABAB] from-10% to-[#D14D72] p-3 rounded-full"
+                >
+                  <FaTwitter />
+                </a>
+                <a
+                  href="https://linkedin.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white bg-gradient-to-l from-[#FFABAB] from-10% to-[#D14D72] p-3 rounded-full"
+                >
+                  <FaLinkedinIn />
+                </a>
+                <a
+                  href="https://instagram.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white bg-gradient-to-l from-[#FFABAB] from-10% to-[#D14D72] p-3 rounded-full"
+                >
+                  <FaInstagram />
+                </a>
+              </div>
+            </div>
+
+            <div className="py-6 text-center md:text-right basis-1/2 lg:basis-1/3 order-2 lg:order-3">
+              <h1 className="text-lg md:text-3xl font-bold">Get in touch:</h1>
               <a
-                href="https://facebook.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white bg-gradient-to-l from-[#FFABAB] from-10% to-[#D14D72] p-3 rounded-full"
+                href="tel:+917696331130"
+                className="flex justify-center md:justify-end text-white hover:underline mt-1 md:mt-2 text-sm md:text-base"
               >
-                <FaFacebookF />
+                <FaPhone className="mr-2 mt-1" />
+                <span>+91-76963-31130</span>
               </a>
               <a
-                href="https://twitter.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white bg-gradient-to-l from-[#FFABAB] from-10% to-[#D14D72] p-3 rounded-full"
+                href="mailto:weddingfilmographer@gmail.com"
+                className="flex justify-center md:justify-end text-white hover:underline mt-1 md:mt-2 text-sm md:text-base"
               >
-                <FaTwitter />
-              </a>
-              <a
-                href="https://linkedin.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white bg-gradient-to-l from-[#FFABAB] from-10% to-[#D14D72] p-3 rounded-full"
-              >
-                <FaLinkedinIn />
-              </a>
-              <a
-                href="https://instagram.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white bg-gradient-to-l from-[#FFABAB] from-10% to-[#D14D72] p-3 rounded-full"
-              >
-                <FaInstagram />
+                <FaEnvelope className="mr-2 mt-1" />
+                <span>weddingfilmographer@gmail.com</span>
               </a>
             </div>
           </div>
-
-          <div className="py-6 text-right basis-1/4">
-            <h1 className="text-3xl font-bold">Get in touch:</h1>
-            <a
-              href="tel:+917696331130"
-              className="flex justify-end text-white hover:underline mt-2"
-            >
-              <FaPhone className="mr-2 mt-1" />
-              <span>+91-76963-31130</span>
-            </a>
-            <a
-              href="mailto:weddingfilmographer@gmail.com"
-              className="flex justify-end text-white hover:underline mt-2"
-            >
-              <FaEnvelope className="mr-2 mt-1" />
-              <span>weddingfilmographer@gmail.com</span>
-            </a>
-          </div>
         </div>
-
-        <div className="mt-3">
+        <div className="absolute top-0 h-full right-0 opacity-40">
+          <img
+            src="/images/gold-rings.png"
+            className="h-full w-full object-cover"
+            alt=""
+          />
+        </div>
+      </footer>
+      <footer>
+        <div className="py-2 bg-[#D14D72] text-white">
           <p className="text-center text-white text-base">
             &copy; 2024 Wedding Filmographer | All Rights Reserved | Crafted
             with &hearts; by{" "}
@@ -88,14 +100,8 @@ const Footer = () => {
             </a>
           </p>
         </div>
-      </div>
-      <div className="absolute top-0 h-full right-0 opacity-40">
-        <img src="/images/gold-rings.png" className="h-full" alt="" />
-      </div>
-      {/* <div className="absolute top-0 h-full left-0 opacity-40 rotate-180">
-        <img src="/images/gold-rings.png" className="h-full" alt="" />
-      </div> */}
-    </footer>
+      </footer>
+    </>
   );
 };
 
