e5735dc6c17468b8053fb774f72d6a88fbc7b200
Arpit Goyal <arpitgoyal126116@gmail.com>
2024-04-15 23:11:01 +0530

    send email

--- src/components/SubComponents/ContactForm.jsx
+++ src/components/SubComponents/ContactForm.jsx
@@ -25,10 +25,7 @@ const ContactForm = () => {
       setIsSubmitting(true);
       // Show sending toast
       toast.info("Sending message...");
-      const response = await axios.post(
-        "http://localhost:3001/send-email",
-        formData
-      );
+      const response = await axios.post("/send-email", formData);
       // Show success toast
       toast.success("Message sent successfully!");
       setFormData({
